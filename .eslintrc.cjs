8c786c2d5354ca7ae0887dbceb0483f697a546cc
Mourner <xmyy239@gmail.com>
2023-12-19 13:59:18 +0800

    模板初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@electron-toolkit',
+    // '@electron-toolkit/eslint-config-ts/eslint-recommended',
+    // '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript'
+  ],
+  rules: {
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
+}
---
+++ electron.vite.config.ts
@@ -0,0 +1,53 @@
+/// <reference types="vitest" />
+// Plugins
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import ViteFonts from 'unplugin-fonts/vite'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+// Utilities
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { fileURLToPath, URL } from 'node:url'
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src/renderer/src', import.meta.url)),
+        '@renderer': resolve('src/renderer/src')
+      },
+      extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
+    },
+    plugins: [
+      vue({
+        template: { transformAssetUrls }
+      }),
+      vuetify({
+        autoImport: true,
+        styles: {
+          configFile: 'src/styles/settings.scss'
+        }
+      }),
+      ViteFonts({
+        google: {
+          families: [
+            {
+              name: 'Roboto',
+              styles: 'wght@100;300;400;500;700;900'
+            }
+          ]
+        }
+      })
+    ],
+    define: { 'process.env': {} },
+    test: {
+      globals: true,
+      environment: 'happy-dom'
+    }
+  }
+})
